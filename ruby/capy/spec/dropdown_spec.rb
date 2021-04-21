5025057380018893e9654511fd1fa5750ddce5a6
Isaquedutra <isaquedutra10@gmail.com>
2021-04-21 15:34:57 -0300

    :page_facing_up: add dropdown

---
+++ ruby/capy/spec/dropdown_spec.rb
@@ -0,0 +1,23 @@
+describe "caixa de seleção", :dropdown do
+  it "item especifico simples" do
+    visit "/dropdown"
+    select("Steve Rogers", from: "dropdown")
+    sleep 2 #temp
+  end
+
+  it "item especifico com o find" do
+    visit "/dropdown"
+
+    drop = find(".avenger-list")
+    drop.find("option", text: "Bruce Banner").select_option
+    sleep 2 #temp
+  end
+
+  it "qualquer item", :sample do
+    visit "/dropdown"
+
+    drop = find(".avenger-list")
+    drop.all("option").sample.select_option
+    sleep 2 #temp
+  end
+end
