01273a25a303c0cf21d19c18e542f6901df6103d
Isaquedutra <isaquedutra10@gmail.com>
2021-04-22 21:41:15 -0300

    :hammer:Add after

--- ruby/capy/spec/dropdown_spec.rb
+++ ruby/capy/spec/dropdown_spec.rb
@@ -5,18 +5,19 @@ describe "caixa de opções", :dropdown do
 
   it "item especifico simples" do
     select("Steve Rogers", from: "dropdown")
-    sleep 2 #temp
   end
 
   it "item especifico com o find" do
     drop = find(".avenger-list")
     drop.find("option", text: "Bruce Banner").select_option
-    sleep 2 #temp
   end
 
   it "qualquer item", :sample do
     drop = find(".avenger-list")
     drop.all("option").sample.select_option
-    sleep 2 #temp
+  end
+
+  after(:each) do
+    sleep 1
   end
 end
